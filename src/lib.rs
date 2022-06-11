fcfc80a0c89baaff282134a02ebad457d00f4d95
alexjesipow <alexander.jesipow@gmail.com>
2022-06-11 10:13:38 +0200

    Add some denies

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 // #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
-// #![cfg_attr(test, deny(rust_2018_idioms))]
-// #![cfg_attr(all(test, feature = "full"), deny(unreachable_pub))]
-// #![cfg_attr(all(test, feature = "full"), deny(warnings))]
-// #![cfg_attr(all(test, feature = "nightly"), feature(test))]
-// #![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(test, deny(rust_2018_idioms))]
+#![cfg_attr(all(test, feature = "full"), deny(unreachable_pub))]
+#![cfg_attr(all(test, feature = "full"), deny(warnings))]
+#![cfg_attr(all(test, feature = "nightly"), feature(test))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 mod client;
 mod connection;
