d0af0264fe7bd9598a5f0da5d738e42b3960e83a
alexjesipow <alexander.jesipow@gmail.com>
2022-07-04 20:29:41 +0200

    Max connection limit test

--- tests/main.rs
+++ tests/main.rs
@@ -1,5 +1,7 @@
 use cached::{Client, ClientConnection, Response, ResponseBody, ResponseBodyGet, Server, Status};
 use std::net::SocketAddr;
+use std::time::Duration;
+use tokio::time::timeout;
 
 async fn run_test_server() -> SocketAddr {
     let host = "127.0.0.1";
@@ -19,8 +21,8 @@ async fn run_test_server() -> SocketAddr {
 #[tokio::test]
 async fn test_getting_a_non_existing_key_fails() {
     let address = run_test_server().await;
-    let pool = ClientConnection::new(address).await;
-    let client = Client::new(pool);
+    let conn = ClientConnection::new(address).await;
+    let client = Client::new(conn);
 
     let key = "ABC".to_string();
     let resp = client.get(key.clone()).await.unwrap();
@@ -30,8 +32,8 @@ async fn test_getting_a_non_existing_key_fails() {
 #[tokio::test]
 async fn test_setting_a_key_works() {
     let address = run_test_server().await;
-    let pool = ClientConnection::new(address).await;
-    let client = Client::new(pool);
+    let conn = ClientConnection::new(address).await;
+    let client = Client::new(conn);
 
     let key = "ABC".to_string();
     let value = "1234".to_string();
@@ -54,8 +56,8 @@ async fn test_setting_a_key_works() {
 #[tokio::test]
 async fn test_setting_the_same_key_twice_fails() {
     let address = run_test_server().await;
-    let pool = ClientConnection::new(address).await;
-    let client = Client::new(pool);
+    let conn = ClientConnection::new(address).await;
+    let client = Client::new(conn);
 
     let key = "ABC".to_string();
     let value = "1234".to_string();
@@ -84,8 +86,8 @@ async fn test_setting_the_same_key_twice_fails() {
 #[tokio::test]
 async fn test_deleting_a_key_works() {
     let address = run_test_server().await;
-    let pool = ClientConnection::new(address).await;
-    let client = Client::new(pool);
+    let conn = ClientConnection::new(address).await;
+    let client = Client::new(conn);
 
     let key = "ABC".to_string();
     let value = "1234".to_string();
@@ -117,8 +119,8 @@ async fn test_deleting_a_key_works() {
 #[tokio::test]
 async fn test_deleting_a_non_existing_key_fails() {
     let address = run_test_server().await;
-    let pool = ClientConnection::new(address).await;
-    let client = Client::new(pool);
+    let conn = ClientConnection::new(address).await;
+    let client = Client::new(conn);
 
     let key = "ABC".to_string();
 
@@ -129,8 +131,8 @@ async fn test_deleting_a_non_existing_key_fails() {
 #[tokio::test]
 async fn test_flushing_works() {
     let address = run_test_server().await;
-    let pool = ClientConnection::new(address).await;
-    let client = Client::new(pool);
+    let conn = ClientConnection::new(address).await;
+    let client = Client::new(conn);
 
     let key = "ABC".to_string();
     let value = "1234".to_string();
@@ -162,8 +164,8 @@ async fn test_flushing_works() {
 #[tokio::test]
 async fn test_setting_and_getting_keys_concurrently_works() {
     let address = run_test_server().await;
-    let pool = ClientConnection::new(address).await;
-    let client = Client::new(pool);
+    let conn = ClientConnection::new(address).await;
+    let client = Client::new(conn);
 
     let key_1 = "ABC".to_string();
     let key_2 = "DEF".to_string();
@@ -198,4 +200,30 @@ async fn test_setting_and_getting_keys_concurrently_works() {
             }))
         )
     );
+}
+
+#[tokio::test]
+async fn test_max_connections_limit() {
+    let address = run_test_server().await;
+    let conn_1 = ClientConnection::new(address).await;
+    let client_1 = Client::new(conn_1);
+    let conn_2 = ClientConnection::new(address).await;
+    let client_2 = Client::new(conn_2);
+
+    let key = "ABC".to_string();
+    let resp = client_1.get(key.clone()).await.unwrap();
+    assert_eq!(resp.status, Status::KeyNotFound);
+
+    // We're expecting a timeout here as only one connection is allowed
+    assert!(
+        timeout(Duration::from_millis(100), client_2.get(key.clone()))
+            .await
+            .is_err()
+    );
+
+    // Once client 1 frees up the connection, client 2 should be able to connect
+    drop(client_1);
+
+    let resp = client_2.get(key.clone()).await.unwrap();
+    assert_eq!(resp.status, Status::KeyNotFound);
 }
