fb94cd5f81e7dd83aa35f50ef534102bafb33d16
alexjesipow <alexander.jesipow@gmail.com>
2022-11-12 11:22:36 +0100

    Add server runner

---
+++ cached-server/src/main.rs
@@ -0,0 +1,32 @@
+use cached::Server;
+use clap::Parser;
+
+const BANNER: &str = r#"
+ ______     ______     ______     __  __     ______     _____
+/\  ___\   /\  __ \   /\  ___\   /\ \_\ \   /\  ___\   /\  __-.
+\ \ \____  \ \  __ \  \ \ \____  \ \  __ \  \ \  __\   \ \ \/\ \
+ \ \_____\  \ \_\ \_\  \ \_____\  \ \_\ \_\  \ \_____\  \ \____-
+  \/_____/   \/_/\/_/   \/_____/   \/_/\/_/   \/_____/   \/____/
+
+"#;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(long)]
+    host: String,
+    #[arg(short, long)]
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+    println!("{}", BANNER);
+
+    let host = cli.host;
+    let addr = format!("{}:{}", host, cli.port);
+    let server = Server::new().bind(addr).await.unwrap();
+    println!("Server running on {}:{}", host, server.port());
+    server.run().await;
+}
