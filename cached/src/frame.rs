79356639413e0613f8e04f151936db025b637500
alexjesipow <alexander.jesipow@gmail.com>
2023-01-28 11:29:57 +0100

    Consolidate frame module

--- cached/src/frame.rs
+++ cached/src/frame.rs
@@ -1,9 +1,73 @@
-use crate::domain::TTLSinceUnixEpochInMillis;
 use crate::error::{Error, FrameError, Result};
-use crate::primitives::{OpCode, StatusCode};
 use bytes::{Buf, Bytes};
+use std::fmt::Debug;
+
+use crate::domain::{Key, TTLSinceUnixEpochInMillis, Value};
+use crate::primitives::OpCode;
+use crate::StatusCode;
 
 static HEADER_SIZE_BYTES: u8 = 23;
+
+#[derive(Debug)]
+pub(crate) struct ResponseFrame {
+    pub header: ResponseHeader,
+    pub key: Option<Key>,
+    pub value: Option<Value>,
+}
+
+impl ResponseFrame {
+    pub(crate) fn new(
+        op_code: OpCode,
+        status: StatusCode,
+        ttl_since_unix_epoch_in_millis: TTLSinceUnixEpochInMillis,
+        key: Option<Key>,
+        value: Option<Value>,
+    ) -> Result<Self> {
+        let key_length = key.as_ref().map_or(0, |k| k.len());
+        let value_length = value.as_ref().map_or(0, |v| v.len());
+        // TODO?
+        // We're assuming no overflow here as value should be sufficiently smaller than u32:MAX - 2*u8::MAX
+        let total_frame_length = ResponseHeader::size() as u32 + key_length as u32 + value_length;
+        let header = ResponseHeader::new(
+            op_code,
+            status,
+            key_length,
+            total_frame_length,
+            ttl_since_unix_epoch_in_millis,
+        );
+        Ok(Self { header, key, value })
+    }
+}
+
+#[derive(Debug)]
+#[cfg_attr(test, derive(PartialEq, Eq))]
+pub(crate) struct RequestFrame {
+    pub header: RequestHeader,
+    pub key: Option<Key>,
+    pub value: Option<Value>,
+}
+
+impl RequestFrame {
+    pub(crate) fn new(
+        op_code: OpCode,
+        ttl_since_unix_epoch_in_millis: TTLSinceUnixEpochInMillis,
+        key: Option<Key>,
+        value: Option<Value>,
+    ) -> Result<Self> {
+        let key_length = key.as_ref().map_or(0, |k| k.len());
+        let value_length = value.as_ref().map_or(0, |v| v.len());
+        // TODO?
+        // We're assuming no overflow here as value should be sufficiently smaller than u32:MAX - 2*u8::MAX
+        let total_frame_length = ResponseHeader::size() as u32 + key_length as u32 + value_length;
+        let header = RequestHeader::new(
+            op_code,
+            key_length,
+            total_frame_length,
+            ttl_since_unix_epoch_in_millis,
+        );
+        Ok(Self { header, key, value })
+    }
+}
 
 #[derive(Debug, Copy, Clone)]
 #[cfg_attr(test, derive(PartialEq, Eq))]
@@ -15,7 +79,7 @@ pub(crate) struct RequestHeader {
 }
 
 impl RequestHeader {
-    pub(crate) fn new(
+    fn new(
         op_code: OpCode,
         key_length: u8,
         total_frame_length: u32,
@@ -45,7 +109,7 @@ pub(crate) struct ResponseHeader {
 }
 
 impl ResponseHeader {
-    pub(crate) fn new(
+    fn new(
         op_code: OpCode,
         status: StatusCode,
         key_length: u8,
